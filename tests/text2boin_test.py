0279c1d08057be80c9fd4485d55c7ce732e81340
AjxLab <abe12@mccc.jp>
2020-04-10 06:18:46 +0900

    test

--- tests/text2boin_test.py
+++ tests/text2boin_test.py
@@ -15,7 +15,6 @@ KATAKANA = (
 class Text2Boin(unittest.TestCase):
     def test_hira2kana(self):
         self.assertEqual(pyboin.text2boin(HIRAGANA[0]), HIRAGANA[1])
-        print('hello')
 
     def test_kana2hira(self):
         self.assertEqual(pyboin.text2boin(KATAKANA[0], cv='katakana'), HIRAGANA[1])
