9f4c26dd26c0b997469a7017a9ecc2b1a8bd28a9
AjxLab <abe12@mccc.jp>
2020-07-14 12:14:52 +0900

    romanize

--- pyboin/pyboin.py
+++ pyboin/pyboin.py
@@ -3,11 +3,11 @@ import jaconv
 
 vowel_map =\
     [
-        ['ア', 'アカサタナハマヤラワガザダバパァャヮ'],
-        ['イ', 'イキシチニヒミリギジヂビピィ'],
-        ['ウ', 'ウクスツヌフムユルグズヅブプゥッュ'],
-        ['エ', 'エケセテネヘメレゲゼデベペェ'],
-        ['オ', 'オコソトノホモヨロヲゴゾドボポォョ'],
+        ['ア', 'アカサタナハマヤラワガザダバパァ　ャヮ'],
+        ['イ', 'イキシチニヒミ　リ　ギジヂビピィ　　　'],
+        ['ウ', 'ウクスツヌフムユル　グズヅブプゥッュ　'],
+        ['エ', 'エケセテネヘメ　レ　ゲゼデベペェ　　　'],
+        ['オ', 'オコソトノホモヨロヲゴゾドボポォ　ョ　'],
     ]
 
 
@@ -21,6 +21,9 @@ def text2boin(text, cv='katakana'):
 
     # replace
     for i, c in enumerate(text):
+        if c == '　':
+            ret += '　'
+
         for pair in vowel_map:
             # match
             if c in pair[1]:
@@ -33,3 +36,20 @@ def text2boin(text, cv='katakana'):
         ret = jaconv.kata2hira(ret)
 
     return ret
+
+
+def romanize(char, vowel):
+    ## -----*----- 母音を変換 -----*----- ##
+    char_romanize = ''
+    vowel_list = ['ア', 'イ', 'ウ', 'エ', 'オ']
+
+    # convert vowel
+    for i in range(len(vowel_map)):
+        for j in range(len(vowel_map[i][1])):
+            if char == vowel_map[i][1][j]:
+                print(vowel_list.index(vowel))
+                char_romanize = vowel_map[vowel_list.index(vowel)][1][j]
+                return char_romanize
+
+    return char
+
